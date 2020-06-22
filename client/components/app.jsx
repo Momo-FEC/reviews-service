067d58f0a88150296e3b0ae84d9fbe76b2a1b2be
Zaim <zaimali10@yahoo.com>
2020-06-22 15:56:27 -0500

    Server side gets the reviews data from server side and sets the data to a state

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      allReviews: []
+    };
   }
 
+  componentDidMount() {
+    serverCommunicator.getReviews()
+      .done(data => {
+        this.setState({
+          allReviews: data
+        });
+      });
+  }
 
   render() {
     return (
@@ -15,6 +27,23 @@ class App extends React.Component {
     );
   }
 }
+
+var serverCommunicator = {
+  getReviews: () => {
+    console.log('getReviews was invoked...');
+    return $.ajax({
+      url: 'http://localhost:3006/api/reviews',
+      method: 'GET',
+      success: (data) => {
+        console.log('we got the reviews', data);
+        return data;
+      },
+      error: (err) => {
+        console.log('failed to GET from server', err)
+      }
+    });
+  }
+};
 
 export default App;
 
