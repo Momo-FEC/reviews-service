4608d963e1afc83edae3dc5e2a92a64eb1a2334e
Zaim <zaimali10@yahoo.com>
2020-06-25 14:24:26 -0500

    Got the header component's information showing. Includes: The number of each overall ratings, overall rating average, feature perfomance design and value averages also calculated and showing

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import $ from 'jquery';
 import ListedReviews from './content.jsx';
-import RatingSnapshot from './header.jsx';
+import { FaStar } from 'react-icons/fa';
+
 
 class App extends React.Component {
   constructor(props) {
@@ -80,13 +81,102 @@ class App extends React.Component {
   }
 
   render() {
+  
+    var count = {
+      total: 0,
+      fiveStar: 0,  
+      fourStar: 0,
+      threeStar: 0,
+      twoStar: 0,
+      oneStar: 0,
+      totalNumber: 0,
+      totalAvg: 0,
+      features: 0,  
+      performance: 0,
+      design: 0,
+      value: 0,
+    };
+
+
+    var counter = () => {
+      this.state.allReviews.forEach((review) => {
+        if (review.overall === 5) {
+          count.total += 1;
+          count.fiveStar += 1;
+          count.totalNumber += 5;
+          count.features += review.feature;
+          count.performance += review.performance;
+          count.design += review.design;
+          count.value += review.value;
+        } else if (review.overall === 4) {
+          count.total += 1;
+          count.fourStar += 1;
+          count.totalNumber += 4;
+          count.features += review.feature;
+          count.performance += review.performance;
+          count.design += review.design;
+          count.value += review.value;
+        } else if (review.overall === 3) {
+          count.total += 1;
+          count.threeStar += 1;
+          count.totalNumber += 3;
+          count.features += review.feature;
+          count.performance += review.performance;
+          count.design += review.design;
+          count.value += review.value;
+        } else if (review.overall === 2) {
+          count.total += 1;
+          count.twoStar += 1;
+          count.totalNumber += 2;
+          count.features += review.feature;
+          count.performance += review.performance;
+          count.design += review.design;
+          count.value += review.value;
+        } else if (review.overall === 1) {
+          count.total += 1;
+          count.oneStar += 1;
+          count.totalNumber += 1;
+          count.features += review.feature;
+          count.performance += review.performance;
+          count.design += review.design;
+          count.value += review.value;
+        }
+      }
+      );
+      count.totalAvg = count.totalNumber / this.state.allReviews.length;
+      count.features = count.features / this.state.allReviews.length;
+      count.performance = count.performance / this.state.allReviews.length;  
+      count.design = count.design / this.state.allReviews.length;  
+      count.value = count.value / this.state.allReviews.length;    
+    };
+
+    counter();
+
+    var stars = <FaStar size={10} color={'#000000'}/>;
+
     return (   
       <div>
 
-        <div className='header'>
-          {this.state.allReviews.map((review) => 
-            <RatingSnapshot key={review.id} review={review} view={this.state.view} overallRating={review.overall}/>
-          )}
+        <div className='RatingSnapshot'>
+          5{stars}{count.fiveStar}
+          <div></div>
+          4{stars}{count.fourStar}
+          <div></div>
+          3{stars}{count.threeStar}
+          <div></div>
+          2{stars}{count.twoStar}
+          <div></div>
+          1{stars}{count.oneStar}
+        </div>
+
+        <div className='AvgCustomerRatings'>
+          <ul>
+            <ul>Overall Average: {count.totalAvg}</ul>
+            <ul>Features Average: {count.features}</ul>
+            <ul>Perfomance Average: {count.performance}</ul>
+            <ul>Design Average: {count.design}</ul>
+            <ul>Value Average: {count.value}</ul>
+          </ul>
         </div>
 
         <div className='listedReviews'>
